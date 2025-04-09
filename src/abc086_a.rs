b48419ce5d326166cbfd65fc68ec3f11b03410af
Yuichi Komorida <nicomathajax32@gmail.com>
2025-04-09 21:23:09 +0900

    Prove abc086_a

--- src/abc086_a.rs
+++ src/abc086_a.rs
@@ -1,5 +1,10 @@
 use creusot_contracts::*;
 
+#[requires(a@ >= 1)]
+#[requires(a@ <= 10000)]
+#[requires(b@ >= 1)]
+#[requires(b@ <= 10000)]
+#[ensures(((a@ * b@) % 2 == 0) == result)]
 pub fn solve(a: usize, b: usize) -> bool {
     (a * b) % 2 == 0
 }
--- src/bin/abc086_a.rs
+++ src/bin/abc086_a.rs
@@ -1,11 +1,19 @@
 use atcoder_with_creusot::abc086_a::*;
-use proconio::input;
+use std::io::{BufWriter, Read, Write, stdin, stdout};
 
 fn main() {
-    input! {
-        a: usize,
-        b: usize,
-    }
+    let mut stdin = stdin();
+    let stdout = stdout();
+    let mut output = BufWriter::new(stdout.lock());
+    let mut input_str = String::new();
+    let _ = stdin.read_to_string(&mut input_str);
+    let mut input_iter = input_str.split_ascii_whitespace();
+    macro_rules! read(() => (input_iter.next().unwrap().parse().unwrap()));
+    macro_rules! print(( $( $x:expr ),*) => (write!(output, $($x,)*).unwrap()));
+    macro_rules! println(( $( $x:expr ),*) => (writeln!(output, $($x,)*).unwrap()));
+
+    let a = read!();
+    let b = read!();
     if solve(a, b) {
         println!("Even");
     } else {
